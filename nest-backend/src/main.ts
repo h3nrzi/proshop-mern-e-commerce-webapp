49f73e3d05cf334da6f8862447c3010628a00b42
h3nrzi <rezaeig22@gmail.com>
2025-12-01 12:16:45 +0330

    refactor: reorganize import statements for consistency and clarity in main application file

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -1,10 +1,10 @@
-import "reflect-metadata";
-import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
-import express from "express";
-import morgan from "morgan";
-import path from "path";
+import * as express from "express";
+import * as morgan from "morgan";
+import * as path from "path";
+import "reflect-metadata";
 import { AppModule } from "./app.module";
 import { MongooseExceptionFilter } from "./common/filters/mongoose-exception.filter";
 
@@ -12,21 +12,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const rootDir = path.resolve();
-  const nodeEnv = process.env.NODE_ENV;
-
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
-  if (nodeEnv === "development") app.use(morgan("dev"));
+  app.use(morgan("dev"));
   app.use("/uploads", express.static(path.join(rootDir, "/uploads")));
+
   app.setGlobalPrefix("api");
+  app.useGlobalFilters(new MongooseExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     })
   );
-  app.useGlobalFilters(new MongooseExceptionFilter());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
