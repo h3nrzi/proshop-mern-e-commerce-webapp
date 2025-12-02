33dc5ecbf70191177e00b25cffb48d511a3634f3
h3nrzi <rezaeig22@gmail.com>
2025-12-02 10:45:45 +0330

    feat: implement file upload module with Multer integration for handling image uploads and validation

---
+++ nest-backend/src/upload/upload.controller.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { AuthGuard } from "../auth/auth.guard";
+import { AdminGuard } from "../auth/admin.guard";
+
+@Controller("upload")
+export class UploadController {
+  @Post()
+  @UseGuards(AuthGuard, AdminGuard)
+  @UseInterceptors(FileInterceptor("image"))
+  uploadFile(@UploadedFile() file?: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException("Invalid file type. Only JPG, JPEG, PNG, and WEBP are allowed.");
+    }
+
+    return {
+      message: "Image Uploaded",
+      image: `/${file.path}`
+    };
+  }
+}
--- nest-backend/src/upload/upload.module.ts
+++ nest-backend/src/upload/upload.module.ts
@@ -1,4 +1,42 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MulterModule } from "@nestjs/platform-express";
+import * as multer from "multer";
+import * as path from "path";
+import { AdminGuard } from "../auth/admin.guard";
+import { AuthGuard } from "../auth/auth.guard";
+import { User, UserSchema } from "../user/user.schema";
+import { UploadController } from "./upload.controller";
 
-@Module({})
+const storage = multer.diskStorage({
+  destination(_req, _file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(_req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const fileFilter = (
+  _req: any,
+  file: Express.Multer.File,
+  cb: (error: Error | null, acceptFile: boolean) => void
+) => {
+  const fileTypes = /jpg|jpeg|png|webp/;
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+  if (!extname || !mimetype) {
+    return cb(new Error("Invalid file type. Only JPG, JPEG, PNG, and WEBP are allowed."), false);
+  }
+  cb(null, true);
+};
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MulterModule.register({ storage, fileFilter }),
+  ],
+  controllers: [UploadController],
+  providers: [AuthGuard, AdminGuard],
+})
 export class UploadModule {}
